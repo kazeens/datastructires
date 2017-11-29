a6ad58265e51b5b8848053b3174c0b2e34eef4e8
kazeens <kolyanbestele@gmail.com>
2017-11-29 22:33:26 +0300

    change codestyle

--- jssource.js
+++ jssource.js
@@ -1,64 +1,74 @@
-class Node{
- constructor(val){
+class Node {
+    constructor(val) {
 
- 	this.val=val;
- 	this.next={};
- }
- toString(){
-    return 	"[ Value: "+String(this.val) +" Next:"+String(this.next.val)+" ]";
-	
-}
+        this.val = val;
+        this.next = {};
+    }
+    toString() {
+        return "[ Value: " + String(this.val) + " Next:" + String(this.next.val) + " ]";
 
-}
-
-
-class Stack{
-
-constructor (){
-
- this.head=null;
+    }
 
 }
 
-enstack(val){
-	let node; 
-	node = new Node(val)
-	node.next=this.head;
-	this.head=node;
-	}
 
-destack(){
-	if(this.head===null){console.log('The stack is already empty'); return;}
-	this.head=this.head.next
-	
-}
+class Stack {
 
-output(){
-	if(this.head===null){console.log('The stack is already empty'); return;}
-	var temp;
-	temp=this.head;
-	while (temp!==null) {
-	console.log(temp.val);
-	temp=temp.next;
-	
-}
+    constructor() {
+
+        this.head = null;
+
+    }
+
+    enstack(val) {
+        let node;
+        node = new Node(val)
+        node.next = this.head;
+        this.head = node;
+    }
+
+    destack() {
+        if (this.head === null) {
+            console.log('The stack is already empty');
+            return;
+        }
+        this.head = this.head.next
+
+    }
+
+    output() {
+        if (this.head === null) {
+            console.log('The stack is already empty');
+            return;
+        }
+        var temp;
+        temp = this.head;
+        while (temp !== null) {
+            console.log(temp.val);
+            temp = temp.next;
+
+        }
+
+    }
+
+    findValue(value) {
+
+        var temp = new Node();
+        temp = this.head;
+        if (temp === null) {
+            return console.log('There is no a such value in the stack');
+        }
+        while (temp.next !== null) {
+            if (temp.val === value) {
+                return console.log('There is a such value in the stack ' + temp.toString());
+            }
+            temp = temp.next;
+        }
+        return console.log('There is no a such value in the stack');
+    }
 
 }
-
-findValue(value){
-
-var temp= new Node();
- temp=this.head;
- if(temp===null){return console.log('There is no a such value in the stack');	}
-while(temp.next!==null){
-if(temp.val===value){ return console.log('There is a such value in the stack '+ temp.toString());}
-temp=temp.next;
-}
-return console.log('There is no a such value in the stack');		
-}
-
-}
-var stp= new Stack();
+var stp = new Stack();
 
 stp.enstack(77);
 stp.enstack(88);
